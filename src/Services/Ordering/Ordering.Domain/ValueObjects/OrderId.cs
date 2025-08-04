e181bcc53e81c5508499f74defba2c020622c97e
madshan14 <madshanofficial@gmail.com>
2025-08-04 12:44:03 -0400

    Strongly Typed IDs

---
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/OrderId.cs
@@ -0,0 +1,17 @@
+﻿namespace Ordering.Domain.ValueObjects
+{
+    public record OrderId
+    {
+        public Guid Value { get; }
+        private OrderId(Guid value) => Value = value;
+        public static OrderId Of(Guid value)
+        {
+            ArgumentNullException.ThrowIfNull(value);
+            if (value == Guid.Empty)
+            {
+                throw new DomainException("Order ID cannot be empty");
+            }
+            return new OrderId(value);
+        }
+    }
+}
